f77afdd13f306c1d9550e1aad1a392a846315293
Holger Kreis <holger@markaspot.org>
2017-05-04 06:51:12 +0200

    Update gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 // Include gulp.
 var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
 var config = require('./config.json');
 
 // Include plugins.
@@ -15,83 +16,67 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
-var scssLint = require('gulp-scss-lint');
-var jshint = require('gulp-jshint');
-var livereload = require('gulp-livereload');
 
 // CSS.
 gulp.task('css', function() {
-    return gulp.src(config.css.src)
-        .pipe(glob())
-        .pipe(plumber({
-            errorHandler: function (error) {
-                notify.onError({
-                    title:    "Gulp",
-                    subtitle: "Failure!",
-                    message:  "Error: <%= error.message %>",
-                    sound:    "Beep"
-                }) (error);
-                this.emit('end');
-            }}))
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            style: 'compressed',
-            errLogToConsole: true,
-            includePaths: config.css.includePaths
-        }))
-        .pipe(autoprefix('last 2 versions', '> 1%', 'ie 9', 'ie 10'))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(config.css.dest))
+  return gulp.src(config.css.src)
+    .pipe(glob())
+    .pipe(plumber({
+      errorHandler: function (error) {
+        notify.onError({
+          title:    "Gulp",
+          subtitle: "Failure!",
+          message:  "Error: <%= error.message %>",
+          sound:    "Beep"
+        }) (error);
+        this.emit('end');
+      }}))
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      style: 'compressed',
+      errLogToConsole: true,
+      includePaths: config.css.includePaths
+    }))
+    .pipe(autoprefix('last 2 versions', '> 1%', 'ie 9', 'ie 10'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(config.css.dest))
+    .pipe(browserSync.reload({ stream: true, match: '**/*.css' }));
 });
 
 // Compress images.
 gulp.task('images', function () {
-    return gulp.src(config.images.src)
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{ removeViewBox: false }],
-            use: [pngcrush()]
-        }))
-        .pipe(gulp.dest(config.images.dest));
+  return gulp.src(config.images.src)
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{ removeViewBox: false }],
+      use: [pngcrush()]
+    }))
+    .pipe(gulp.dest(config.images.dest));
 });
 
 // Fonts.
 gulp.task('fonts', function() {
-    return gulp.src(config.fonts.src)
-        .pipe(gulp.dest(config.fonts.dest));
+  return gulp.src(config.fonts.src)
+    .pipe(gulp.dest(config.fonts.dest));
 });
 
 // Watch task.
 gulp.task('watch', function() {
-    livereload.listen();
-    gulp.watch(config.css.src, ['css']);
-    gulp.watch(config.images.src, ['images']);
+  gulp.watch(config.css.src, ['css']);
+  gulp.watch(config.images.src, ['images']);
 });
 
 // Static Server + Watch
 gulp.task('serve', ['css', 'fonts', 'watch'], function() {
-
+  browserSync.init({
+    proxy: config.proxy
+  });
 });
 
 // Run drush to clear the theme registry.
 gulp.task('drush', shell.task([
-    'drush cache-clear theme-registry'
+  'drush cache-clear theme-registry'
 ]));
-
-// SCSS Linting.
-gulp.task('scss-lint', function() {
-    return gulp.src([config.css.src])
-        .pipe(scssLint())
-        .pipe(scssLint.format())
-        .pipe(scssLint.failOnError());
-});
-
-// JS Linting.
-gulp.task('js-lint', function() {
-    return gulp.src(config.js.src)
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
-});
 
 // Default Task
 gulp.task('default', ['serve']);
